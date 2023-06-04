ddcbbd36779fa2ff61ba58c9a162c2fb99044afc
tynrare <tynrare@gmail.com>
2023-06-04 21:25:21 +0300

    fc

---
+++ serve.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+import os
+
+print(os.getcwd())
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
